53750b6f3784b6f70a6c5910189e1091db2bf82d
jdavidduart <77544911+jdavidduart@users.noreply.github.com>
2021-08-07 19:39:14 -0500

    pagination interactive

--- countries/src/Components/Home/Home.js
+++ countries/src/Components/Home/Home.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.scss';
 import Card from '../Card/Card';
 import Loading from '../Loading/Loading'
+import Pagination from '../Paginate/Pagination';
+
+
 export default function Home({allInfo}){
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const postsPerPage = 16;
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    let currentPosts = allInfo.currentInfo.slice(indexOfFirstPost, indexOfLastPost);
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+
     if(allInfo.loading === true) return <Loading/>
     else{
         return(
             <div className='homeWrap'>
                 <div className='cardsContainer'>
                     {
-                        allInfo.currentInfo?.map( (country, index) =>(
+                        currentPosts.map( (country, index) =>(
                             <Card country={country} key={index}/>
                             ))
                         }
                 </div>
+                <Pagination postsPerPage={postsPerPage} totalPosts={allInfo.currentInfo.length} paginate={paginate}/>
             </div>
          )
     }
