a5fb9ca51670f2a3193065c4f337efd6ef5b049c
jdavidduart <77544911+jdavidduart@users.noreply.github.com>
2021-08-08 10:49:20 -0500

    deploy and Readme

--- countries/src/Components/Card/Card.js
+++ countries/src/Components/Card/Card.js
@@ -18,7 +18,7 @@ export default function Card ({country}){
                     <h3 className='cardTitle'>{country.name}</h3>
                     <div className='capital'>Capital: {country.capital}</div>
                     <div className='area'><FcLandscape className='iconCard'/> {country.area} km<sup>2</sup></div>
-                    <div><FcConferenceCall className='iconCard'/> {country.population} Habitants</div>
+                    <div><FcConferenceCall className='iconCard'/> {country.population} H</div>
                     <div><FcCallback className='iconCard'/> +{country.callingCodes[0]}</div>
                     
                 </div>
